cff79c3717e3fa867a468d8783cc6cc5574186a3
Pär Svanström <svanstrom@gmail.com>
2025-12-14 20:48:44 +0100

    add state classes

--- components/cf_echo2/sensor.py
+++ components/cf_echo2/sensor.py
@@ -1,7 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
-from esphome.const import CONF_ID
+from esphome.const import (
+    CONF_ID,
+    DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_VOLUME,
+    STATE_CLASS_MEASUREMENT,
+    STATE_CLASS_TOTAL_INCREASING,
+)
 
 CONF_ENERGY = "energy"
 CONF_VOLUME = "volume"
@@ -17,21 +25,50 @@ AUTO_LOAD = ["sensor"]
 cf_echo2_ns = cg.esphome_ns.namespace("cf_echo2")
 CFEcho2Reader = cf_echo2_ns.class_("CFEcho2Reader", cg.PollingComponent, uart.UARTDevice)
 
-SENSOR_SCHEMA = sensor.sensor_schema(accuracy_decimals=3)
-TEMP_SENSOR_SCHEMA = sensor.sensor_schema(accuracy_decimals=1, unit_of_measurement="°C")
+SENSOR_SCHEMA = sensor.sensor_schema(
+    accuracy_decimals=3,
+    state_class=STATE_CLASS_TOTAL_INCREASING,
+    device_class=DEVICE_CLASS_ENERGY,
+    unit_of_measurement="kWh",
+)
+VOLUME_SCHEMA = sensor.sensor_schema(
+    accuracy_decimals=3,
+    state_class=STATE_CLASS_TOTAL_INCREASING,
+    device_class=DEVICE_CLASS_VOLUME,
+    unit_of_measurement="m³",
+)
+POWER_SCHEMA = sensor.sensor_schema(
+    accuracy_decimals=3,
+    state_class=STATE_CLASS_MEASUREMENT,
+    device_class=DEVICE_CLASS_POWER,
+    unit_of_measurement="W",
+)
+VOLUME_FLOW_SCHEMA = sensor.sensor_schema(
+    accuracy_decimals=3,
+    state_class=STATE_CLASS_MEASUREMENT,
+    unit_of_measurement="m³/h",
+)
+TEMP_SENSOR_SCHEMA = sensor.sensor_schema(
+    accuracy_decimals=1,
+    unit_of_measurement="°C",
+    device_class=DEVICE_CLASS_TEMPERATURE,
+    state_class=STATE_CLASS_MEASUREMENT,
+)
 
 CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(CFEcho2Reader),
             cv.Optional(CONF_ENERGY): SENSOR_SCHEMA,
-            cv.Optional(CONF_VOLUME): SENSOR_SCHEMA,
-            cv.Optional(CONF_POWER): SENSOR_SCHEMA,
-            cv.Optional(CONF_VOLUME_FLOW): sensor.sensor_schema(accuracy_decimals=3, unit_of_measurement="m³/h"),
+            cv.Optional(CONF_VOLUME): VOLUME_SCHEMA,
+            cv.Optional(CONF_POWER): POWER_SCHEMA,
+            cv.Optional(CONF_VOLUME_FLOW): VOLUME_FLOW_SCHEMA,
             cv.Optional(CONF_FLOW_TEMP): TEMP_SENSOR_SCHEMA,
             cv.Optional(CONF_RETURN_TEMP): TEMP_SENSOR_SCHEMA,
             cv.Optional(CONF_DELTA_T): sensor.sensor_schema(
-                accuracy_decimals=1, unit_of_measurement="K"
+                accuracy_decimals=1,
+                unit_of_measurement="K",
+                state_class=STATE_CLASS_MEASUREMENT,
             ),
         }
     )
